fb50e4b51a2c605661649ae38f5f4edb9e1d640a
ilyab <ilya.bubnoff@yandex.ru>
2025-02-04 21:44:29 +0300

    Decided

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Задача 1");
+        int[] arr = {12000, 17800, 15300, 14346, 11432};
+        float sum = 0;
+        for (int index = 0; index < arr.length; index++) {
+            sum += arr[index];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+        System.out.println("Задача 2");
+        int min = arr[0];
+        int max = -1;
+        for (int current : arr) {
+            if (current < min){
+                min = current;
+            }
+            if (current > max) {
+                max = current;
+            }
+        }
+            System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей.");
+            System.out.println("Максимальная сумма трат за неделю составила " + max + " рублей.");
+        System.out.println("Задача 3");
+        float average = 0;
+        for (int current : arr){
+            average = sum / arr.length;
+        }
+            System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
+        System.out.println("Задача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for(int i = reverseFullName.length-1;i >= 0; i--){
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
